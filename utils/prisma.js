714a0341f878f54a0c201af6ca9135f51a3a571c
mteverest182 <mteverest182@gmail.com>
2026-09-07 21:35:19 +0700

    Fix Prisma client for JavaScript runtime

--- utils/prisma.js
+++ utils/prisma.js
@@ -2,7 +2,7 @@ import "dotenv/config";
 
 import { Pool } from "pg";
 import { PrismaPg } from "@prisma/adapter-pg";
-import { PrismaClient } from "../generated/prisma/client.js";
+import { PrismaClient } from "@prisma/client";
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
